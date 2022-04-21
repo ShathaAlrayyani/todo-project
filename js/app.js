8c22fd3f4c11a2a714ed1f879d0bbac96b1c0ddb
Shatha Alrayyani <shatha.alrayyani223@gmail.com>
2022-04-21 04:01:45 +0300

    adding 3 Questions

--- js/app.js
+++ js/app.js
@@ -20,4 +20,36 @@
      alert("thanks");
  }
 
+ let userAnswer1= prompt( "Q1: are you looking for a job ?");
+
+ function firstQestion(){
+  if (userAnswer1==""){
+    alert ("your answer for Q1 invalid")
+  }
+ }
+
+ firstQestion()
+
+ let userAnswer2= prompt("Q2: are you a software developer ? ");
+
+ function secQestion(){
+  if (userAnswer1==""){
+    alert ("your answer for Q2 invalid")
+  }
+ }
+ secQestion()
+
+ let userAnswer3= prompt("Q3: Do you mind working as a freelancer ?");
+
+ function thirdQestion(){
+  if (userAnswer1==''){
+    alert ("your answer for Q3 invalid")
+  }
+ }
+ thirdQestion()
+
+ let answers = [userAnswer1,userAnswer2,userAnswer3]
+  console.log(answers);
+
+  
 
